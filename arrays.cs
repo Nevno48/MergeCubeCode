d625217b8ffe35c75b2ae3ba76e7a43bf61f9c31
Nevno48 <nevno48@gmail.com>
2018-05-31 14:21:09 -0400

    Add files via upload

---
+++ arrays.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randNumArray;
+            int[] randArray = new int[5];
+            int[] randArray2 = { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Array Length: " + randArray2.Length);
+            Console.WriteLine("Item 0: " + randArray2[0]);
+
+            //loops
+            for(int x = 0; x < randArray2.Length; x++)
+            {
+                Console.WriteLine("[{0}:{1}", x, randArray[x]);
+            }
+            foreach (int num in randArray2)
+            {
+                Console.WriteLine(num);
+            }
+
+            //multi-Dimensional
+            int[,] multArray = new int[5, 3];
+            int[,] multArray2 = { { 0, 1 }, { 2, 3 }, { 4, 5 } };
+
+            foreach (int num in multArray2)
+            {
+                Console.WriteLine(num);
+            }
+            for(int x = 0; x <  multArray2.GetLength(0); x++)
+            {
+                for(int y = 0; y < multArray2.GetLength(1); y++)
+                {
+                    Console.WriteLine("{0} | {1}: {2}", x, y, multArray2[x,y]);
+                }
+            }
+
+        }
+    }
+}
---
+++ conditionals.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secondProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Typecasting");
+            double numPi = 3.14;
+            int wordPi = (int)numPi;
+
+            Random rand = new Random();
+            Console.WriteLine("Random number between 1 and 10 :" + rand.Next(1, 11));
+
+            int age = 17;
+            if ((age >= 5) && (age <= 7))
+            {
+                Console.WriteLine("Go to Elementary School");
+
+            }
+            else if ((age > 7) && (age < 13))
+            {
+                Console.WriteLine("Go to Middle School");
+
+            }
+            else
+            {
+                Console.Write("Go to High School.");
+            }
+            //assigns the value of canDrive as a value of 1 or 2 depending on the value of the condition
+            int canDrive = age >= 16 ? 1 : 0;
+
+            switch(age)
+            {
+                case 0:
+                    Console.WriteLine("infant");
+                    break;
+                case 1:
+                case 2:
+                    Console.WriteLine("Go to elementary School.");
+                    goto Cute;
+                default:
+                    Console.WriteLine("Child");
+                    break;
+
+            }
+
+            Cute:
+            Console.WriteLine("Toddlers are cute");
+
+        }
+    }
+}
